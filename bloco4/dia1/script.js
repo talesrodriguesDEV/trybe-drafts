c5aae5bb36ea3957afaf8c6ead8cc15330e8881d
Tales Rodrigues <tales00001@gmail.com>
2022-04-06 09:30:14 -0300

    adiciona prática 1

---
+++ bloco4/dia1/script.js
@@ -0,0 +1,11 @@
+let myName = "Tales";
+let birthCity = "Goiânia";
+let birthYear = 2001;
+
+console.log("Meu nome é " + myName + ", eu nasci em " + birthCity + " no ano de " + birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Itajaí";
+console.log(birthCity);

e1b0386ebb14c87256f50c0b2b4be58b565b4bdd
Tales Rodrigues <tales00001@gmail.com>
2022-04-06 13:26:38 -0300

    adiciona prática 4

--- bloco4/dia1/script.js
+++ bloco4/dia1/script.js
@@ -45,3 +45,43 @@
 // } else {
 //     console.log("Você foi reprovada(o).");
 // }
+
+// const currentHour = 4;
+// let message = "";
+
+// if (currentHour >= 20){
+//     message = "Não deveríamos comer nada, é hora de dormir.";
+// } else if (currentHour >= 18 && currentHour < 22) {
+//     message = "Rango da noite, vamos jantar :D";
+// } else if (currentHour >= 14 && currentHour < 18) {
+//     message = "Vamos fazer um bolo pro café da tarde?";
+// } else if (currentHour > 11 && currentHour < 14) {
+//     message = "Hora do almoço!!!";
+// } else if (currentHour > 4 && currentHour < 11) {
+//     message = "Hmmm, cheiro de café recém passado";
+// }
+
+// console.log(message);
+
+// let weekDay = "sábado"
+
+// if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+//     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+// } else {
+//     console.log("FINALMENTE, descando merecido UwU");
+// }
+
+let resultado = "lista";
+switch (resultado) {
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!");
+        break;
+    case "lista":
+        console.log("Você está na nossa lista de espera.");
+        break;
+    case "reprovada":
+        console.log("Você foi reprovada(o).");
+        break;
+    default:
+        console.log("Não se aplica");
+}

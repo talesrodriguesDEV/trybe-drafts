11bc4346cb8365c72c09c0a63b5de10ff82b9e12
Tales Rodrigues <tales00001@gmail.com>
2022-04-07 09:16:06 -0300

    adiciona exercícios parte 2

--- bloco4/dia2/script.js
+++ bloco4/dia2/script.js
@@ -1,14 +1,17 @@
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 // let menuServices = menu[1];
-
 // console.log(menuServices);
 
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 // let indexOfPortfolio = menu.indexOf("Portfólio");
-
 // console.log(indexOfPortfolio);
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push("Contato");
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push("Contato");
+// console.log(menu);
 
-console.log(menu);
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for (i = 0; i < groceryList.length; i+= 1) {
+//     console.log(groceryList[i]);
+// }
+

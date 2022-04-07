7c676eb8e570486dff9cef77d22c0a0d9bb1e0ba
Tales Rodrigues <tales00001@gmail.com>
2022-04-07 09:22:10 -0300

    adiciona parte 3

--- bloco4/dia2/script.js
+++ bloco4/dia2/script.js
@@ -1,11 +1,9 @@
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 // let menuServices = menu[1];
 // console.log(menuServices);
-
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 // let indexOfPortfolio = menu.indexOf("Portfólio");
 // console.log(indexOfPortfolio);
-
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 // menu.push("Contato");
 // console.log(menu);
@@ -14,4 +12,30 @@
 // for (i = 0; i < groceryList.length; i+= 1) {
 //     console.log(groceryList[i]);
 // }
+
+
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//   console.log(numero);
+// }
+// let word = 'Hello';
+// for (let letter of word) {
+//   console.log(letter);
+// }
+// let arrOfNumbers = [10, 20, 30];
+// for (let sum of arrOfNumbers) {
+//   sum += 1;
+//   console.log(sum);
+// }
+// console.log(arrOfNumbers);
+// let meuNome = "Tales";
+// for (letras of meuNome){
+//     console.log(letras);
+// }
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names){
+    console.log(name);
+}
+
+
 

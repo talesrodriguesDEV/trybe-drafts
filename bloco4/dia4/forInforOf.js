217386387c50be32fa9691409145fe0fb63d73b9
Tales Rodrigues <tales00001@gmail.com>
2022-04-11 13:09:32 -0300

    adiciona conteúdo parte 2 - for/in e for/of

---
+++ bloco4/dia4/forInforOf.js
@@ -0,0 +1,41 @@
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(index, cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let value in food) {
+//   console.log(value);
+// };
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+// };
+
+// for(let key in names) {
+//     console.log('Olá, ' + names[key] + '!');
+// } 
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for(let key in car) {
+    console.log(key + ': ' + car[key]);
+}
+

b8eeb42e997fdfc6e24013eed3bb6d5dfb32fde1
Tales Rodrigues <tales00001@gmail.com>
2022-04-14 09:45:26 -0300

    adiciona exercícios parte II

--- bloco5/dia2/script.js
+++ bloco5/dia2/script.js
@@ -2,6 +2,7 @@
 // console.log(document.getElementById('start').nextSibling); // nó
 // console.log(document.getElementById('start').nextElementSibling); // <p>elemento</p>
 
+// Parte I
 // 1
 console.log(document.getElementById('elementoOndeVoceEsta'));
 // 2
@@ -18,3 +19,20 @@ console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
 console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
 // 8
 console.log(document.getElementById('pai').firstElementChild.nextElementSibling.nextElementSibling);
+
+// Parte II
+// 1
+let newBrother = document.createElement('section');
+document.querySelector('#elementoOndeVoceEsta').parentNode.appendChild(newBrother);
+newBrother.innerText = 'Novo irmão!';
+console.log(document.querySelector('#elementoOndeVoceEsta').parentNode.children);
+// 2
+let newChild = document.createElement('p');
+document.querySelector('#elementoOndeVoceEsta').appendChild(newChild);
+newChild.innerText = 'Novo filho!';
+// 3
+let primeiroFilhoDoFilhoDoFilho = document.createElement('h1');
+document.querySelector('#primeiroFilhoDoFilho').appendChild(primeiroFilhoDoFilhoDoFilho);
+primeiroFilhoDoFilhoDoFilho.innerText = 'Primeiro filho do filho do filho';
+// 4
+console.log(primeiroFilhoDoFilhoDoFilho.parentNode.parentNode.nextElementSibling);

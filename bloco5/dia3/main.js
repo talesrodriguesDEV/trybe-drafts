b8d3106a59c560b9e62b41390833c7abe4f0c9e9
Tales Rodrigues <tales00001@gmail.com>
2022-04-18 09:32:36 -0300

    Resolução do exercícios 1 e 2

--- bloco5/dia3/main.js
+++ bloco5/dia3/main.js
@@ -8,9 +8,21 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// Porque ela pertence à classe 'tech', que adiciona um deslocamento vertical através da propriedade 'transform'.
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+let liS = document.querySelectorAll('li');
+for (index = 0; index < liS.length; index += 1) {
+    liS[index].addEventListener('click', addTech);
+}
+function addTech(event) {
+    for (index = 0; index < liS.length; index += 1) {
+        liS[index].classList.remove('tech');    
+    }
+    event.target.className = 'tech';
+    // console.log('Oi!');
+}
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
